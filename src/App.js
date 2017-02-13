4de9ab46f9e8ea07290f1a53bcdf88423a9f02cc
David Hérault <dherault@gmail.com>
2017-02-13 18:14:55 +0100

    closure based system

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-// import { createStore } from 'redux';
 import './App.css';
 
 class App extends Component {
 
   componentDidMount() {
-    canvasGraph(document.getElementById('canvas'), window.innerWidth - 40, window.innerHeight - 40);
+    canvasGraph(document.getElementById('canvas'), window.innerWidth - 40, window.innerHeight - 40).start();
   }
 
   render() {
@@ -17,76 +16,83 @@ class App extends Component {
   }
 }
 
-// function entitiesReducer(state = {}, action) {
-//   switch (action.type) {
-//     case 'CREATE_ENTITY':
-//
-//       break;
-//     default:
-//       return state;
-//   }
-// }
-
 function canvasGraph(canvasElement, width, height) {
-  console.log('canvasGraph');
+  console.log('initializing canvasGraph');
 
   const _ = canvasElement.getContext('2d');
-  const state = {
-    mousePos: { x: 0, y: 0 },
-    elementCreators: [],
-  };
 
   _.canvas.width = width;
   _.canvas.height = height;
 
+  const elements = [];
+  const state = {
+    mousePos: { x: 0, y: 0 },
+  };
+
+  function createElement(fn) {
+    elements.push(fn(_));
+  }
+
+  // Not reactive programming yet
   canvasElement.addEventListener('mousemove', e => {
     state.mousePos.x = e.offsetX;
     state.mousePos.y = e.offsetY;
   });
 
-  state.elementCreators.push(_ => {
-    _.fillStyle = '#04070D';
-    _.fillRect(0, 0, width, height);
+  canvasElement.addEventListener('click', e => {
+    const { offsetX, offsetY } = e;
+
+    const clickedElements = elements.filter(el => el.onClick && el.isClicked && el.isClicked(offsetX, offsetY))
+
+    if (clickedElements.length) clickedElements[clickedElements.length - 1].onClick(e);
   });
+
+  createElement(_ => ({
+    id: 'background',
+    draw() {
+      _.fillStyle = '#04070D';
+      _.fillRect(0, 0, width, height);
+    },
+  }))
 
   function getRandom(min, max) {
     return Math.random() * (max - min) + min;
   }
 
   for (let i = 200; i > 0; i--) {
-    let w = width * Math.random();
-    let h = height * Math.random();
-    let ww = w;
-    let hh = h;
-    const r = 70 * Math.random();
-    const t = 50 * Math.random();
-    const k = Math.max(50, 200 * Math.random());
-    const v = 5 * r;
-    const o = Math.floor(1000 * Math.random());
-
-    let temperature = 0;
     // const c = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    createElement(_ => {
+      let clickedState = false;
+      const w = width * Math.random();
+      const h = height * Math.random();
+      const r = 70 * Math.random();
 
-    state.elementCreators.push((_, tick) => {
-      w += (ww - w) / v;
-      h += (hh - h) / v;
+      return {
+        id: Math.random(),
+        zIndex: 5000 * Math.random(),
+        draw() {
+          _.beginPath();
+          _.arc(w, h, r, 0, 2 * Math.PI);
 
-      if (!(tick % o)) {
-        ww += (Math.random() > 0.5 ? -1 : 1) * getRandom(0, 100 * Math.random());
-        hh += (Math.random() > 0.5 ? -1 : 1) * getRandom(0, 100 * Math.random());
-      }
-
-      _.strokeStyle = '#fff';
-      _.beginPath();
-      _.arc(w, h, r * Math.abs(Math.cos(tick / k + t)), 0, 2 * Math.PI);
-      _.stroke();
+          if (clickedState) {
+            _.fillStyle = '#fff';
+            _.fill();
+          }
+          else {
+            _.strokeStyle = '#fff';
+            _.stroke();
+          }
+        },
+        isClicked: (x, y) => Math.pow(Math.abs(w - x), 2) + Math.pow(Math.abs(h - y), 2) <= r * r,
+        onClick: () => clickedState = !clickedState,
+      };
     });
   }
 
   function draw(tick) {
     _.clearRect(0, 0, width, height);
 
-    state.elementCreators.forEach(fn => fn(_, tick));
+    elements.forEach(el => el.draw(tick));
 
     _.fillStyle = '#f00';
 
@@ -95,11 +101,14 @@ function canvasGraph(canvasElement, width, height) {
     _.fill();
   }
 
-  let i = 0;
+  let intervalId;
+  let t = 0;
+  const redrawPeriod = 50;
 
-  setInterval(() => draw(i++), 10);
-
-  draw();
+  return {
+    start: () => intervalId = setInterval(() => draw(t += redrawPeriod), redrawPeriod),
+    stop: () => clearInterval(intervalId),
+  };
 }
 
 export default App;
